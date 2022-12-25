4ef4b7258caf5c891229c993988fd7c4b82b1bda
Moshe Atlow <moshe@atlow.co.il>
2022-12-25 21:31:03 +0200

    test: fixes

--- packages/junit/tests/index.test.js
+++ packages/junit/tests/index.test.js
@@ -2,6 +2,6 @@ const { spawnSync } = require('child_process');
 const { compareLines } = require('../../../tests/utils');
 const output = require('./output');
 
-const child = spawnSync('../../../node/node', ['--test-reporter', './index.js', '../../tests/example']);
+const child = spawnSync('node', ['--test-reporter', './index.js', '../../tests/example']);
 const stdout = child.stdout?.toString();
 compareLines(stdout, output.stdout);
