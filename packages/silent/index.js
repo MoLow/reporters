551b97c0746dd6a10f59819fad2d4b9697b0f3df
Moshe Atlow <moshe@atlow.co.il>
2022-12-26 00:27:48 +0200

    fix: now it works

--- packages/silent/index.js
+++ packages/silent/index.js
@@ -1,2 +1,5 @@
-// eslint-disable-next-line no-empty-function
-module.exports = async function* silent() {};
+// eslint-disable-next-line require-yield
+module.exports = async function* silent(source) {
+  // eslint-disable-next-line no-unused-vars
+  for await (const event of source);
+};
