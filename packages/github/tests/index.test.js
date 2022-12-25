966bae424fb043b31d6c8aa8e2e81e4c330a0e10
Moshe Atlow <moshe@atlow.co.il>
2022-12-25 21:55:11 +0200

    test: use prebuilt binary

--- packages/github/tests/index.test.js
+++ packages/github/tests/index.test.js
@@ -1,6 +1,7 @@
 const { spawnSync } = require('child_process');
 const { tmpdir } = require('os');
 const { join } = require('path');
+const assert = require('assert');
 const { readFileSync, writeFileSync } = require('fs');
 const { compareLines } = require('../../../tests/utils');
 const output = require('./output');
@@ -8,11 +9,10 @@ const output = require('./output');
 const GITHUB_STEP_SUMMARY = join(tmpdir(), 'github-actions-test-reporter');
 writeFileSync(GITHUB_STEP_SUMMARY, '');
 
-const child = spawnSync('node', ['--test-reporter', './index.js', '../../tests/example'], {
+const child = spawnSync(process.execPath, ['--test-reporter', './index.js', '../../tests/example'], {
   env: { GITHUB_STEP_SUMMARY },
 });
-const stdout = child.stdout?.toString();
-const summary = readFileSync(GITHUB_STEP_SUMMARY).toString();
 
-compareLines(stdout, output.stdout);
-compareLines(summary, output.summary);
+assert.strictEqual(child.stderr?.toString(), '');
+compareLines(child.stdout?.toString(), output.stdout);
+compareLines(readFileSync(GITHUB_STEP_SUMMARY).toString(), output.summary);
--- packages/junit/tests/index.test.js
+++ packages/junit/tests/index.test.js
@@ -1,7 +1,9 @@
 const { spawnSync } = require('child_process');
+const assert = require('assert');
 const { compareLines } = require('../../../tests/utils');
 const output = require('./output');
 
-const child = spawnSync('node', ['--test-reporter', './index.js', '../../tests/example']);
-const stdout = child.stdout?.toString();
-compareLines(stdout, output.stdout);
+const child = spawnSync(process.execPath, ['--test-reporter', './index.js', '../../tests/example']);
+
+assert.strictEqual(child.stderr?.toString(), '');
+compareLines(child.stdout?.toString(), output.stdout);
