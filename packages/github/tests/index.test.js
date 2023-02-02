4c709222734de88af71c5a086622c3b022d51fd5
Moshe Atlow <moshe@atlow.co.il>
2023-02-02 20:59:53 +0200

    feat: use reported file (#30)

--- packages/github/tests/index.test.js
+++ packages/github/tests/index.test.js
@@ -2,6 +2,7 @@ const { spawnSync } = require('child_process');
 const { tmpdir } = require('os');
 const { join } = require('path');
 const assert = require('assert');
+const path = require('path');
 const { readFileSync, writeFileSync } = require('fs');
 const { compareLines } = require('../../../tests/utils');
 const output = require('./output');
@@ -10,7 +11,7 @@ const GITHUB_STEP_SUMMARY = join(tmpdir(), 'github-actions-test-reporter');
 writeFileSync(GITHUB_STEP_SUMMARY, '');
 
 const child = spawnSync(process.execPath, ['--test-reporter', './index.js', '../../tests/example'], {
-  env: { GITHUB_STEP_SUMMARY },
+  env: { GITHUB_STEP_SUMMARY, GITHUB_WORKSPACE: path.resolve(__dirname, '../../../') },
 });
 
 assert.strictEqual(child.stderr?.toString(), '');
