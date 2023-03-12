72359c3983334d7712c4d404a0ae297b9ef9e859
Moshe Atlow <moshe@atlow.co.il>
2023-03-12 12:01:44 +0200

    fix(junit): support more usecases

--- packages/junit/index.js
+++ packages/junit/index.js
@@ -1,4 +1,3 @@
-const assert = require('node:assert');
 const util = require('node:util');
 const { hostname } = require('node:os');
 
@@ -14,7 +13,7 @@ function escapeContent(s = '') {
 
 function treeToXML(tree) {
   if (typeof tree === 'string') {
-    return escapeContent(tree);
+    return `${escapeContent(tree)}\n`;
   }
   const {
     tag, props, nesting, children,
@@ -22,12 +21,12 @@ function treeToXML(tree) {
   const propsString = Object.entries(props)
     .map(([key, value]) => `${key}="${escapeProperty(String(value))}"`)
     .join(' ');
-  const indent = '  '.repeat(nesting + 1);
-  const newLine = children?.length ? '\n' : '';
-  const postIndent = newLine ? indent : '';
-  const postNewLine = newLine ? '' : '\n';
-  const childrenString = `${newLine}${(children ?? []).map(treeToXML).join('')}${newLine}${postIndent}`;
-  return `${indent}<${tag} ${propsString}>${childrenString}</${tag}>${postNewLine}`;
+  const indent = '\t'.repeat(nesting + 1);
+  if (!children?.length) {
+    return `${indent}<${tag} ${propsString}/>\n`;
+  }
+  const childrenString = `${(children ?? []).map(treeToXML).join('')}`;
+  return `${indent}<${tag} ${propsString}>\n${childrenString}${indent}</${tag}>\n`;
 }
 
 function isFailure(node) {
@@ -42,27 +41,38 @@ module.exports = async function* junitReporter(source) {
   yield '<?xml version="1.0" encoding="utf-8"?>\n';
   yield '<testsuites>\n';
   let currentSuite = null;
+
+  function startTest(event) {
+    const originalSuite = currentSuite;
+    currentSuite = {
+      props: { name: event.data.name },
+      nesting: event.data.nesting,
+      parent: currentSuite,
+      children: [],
+    };
+    originalSuite?.children.push(currentSuite);
+  }
+
   for await (const event of source) {
     switch (event.type) {
       case 'test:start': {
-        const originalSuite = currentSuite;
-        currentSuite = {
-          props: { name: event.data.name },
-          nesting: event.data.nesting,
-          parent: currentSuite,
-          children: [],
-        };
-        originalSuite?.children.push(currentSuite);
+        startTest(event);
         break;
       }
       case 'test:pass':
       case 'test:fail': {
+        if (!currentSuite) {
+          startTest({ data: { name: 'root', nesting: 0 } });
+        }
+        if (currentSuite.props.name !== event.data.name
+          || currentSuite.nesting !== event.data.nesting) {
+          startTest(event);
+        }
         const currentTest = currentSuite;
         if (currentSuite?.nesting === event.data.nesting && currentSuite?.parent) {
           currentSuite = currentSuite.parent;
         }
-        assert.strictEqual(currentTest.props.name, event.data.name);
-        currentTest.props.time = (event.data.details.duration_ms / 1000).toFixed(5);
+        currentTest.props.time = (event.data.details.duration_ms / 1000).toFixed(6);
         if (currentTest.children.length > 0) {
           currentTest.tag = 'testsuite';
           currentTest.props.disabled = 0;
@@ -73,19 +83,19 @@ module.exports = async function* junitReporter(source) {
           currentTest.props.hostname = HOSTNAME;
         } else {
           currentTest.tag = 'testcase';
-          currentTest.props.classname = 'test';
+          currentTest.props.classname = event.data.classname ?? 'test';
           if (event.data.skip) {
-            currentTest.children.push({ nesting: event.data.nesting + 1, tag: 'skipped', props: { message: event.data.skip } });
+            currentTest.children.push({ nesting: event.data.nesting + 1, tag: 'skipped', props: { type: 'skipped', message: event.data.skip } });
           }
           if (event.data.todo) {
-            currentTest.children.push({ nesting: event.data.nesting + 1, tag: 'skipped', props: { message: event.data.todo } });
+            currentTest.children.push({ nesting: event.data.nesting + 1, tag: 'skipped', props: { type: 'todo', message: event.data.todo } });
           }
           if (event.type === 'test:fail') {
             const error = event.data.details?.error;
             currentTest.children.push({
               nesting: event.data.nesting + 1,
               tag: 'failure',
-              props: { message: error?.message ?? '', type: error.failureType || error.code },
+              props: { type: error?.failureType || error?.code, message: error?.message ?? '' },
               children: [util.inspect(
                 event.data.details?.error,
                 { colors: false, breakLength: Infinity },
@@ -104,5 +114,5 @@ module.exports = async function* junitReporter(source) {
     }
   }
   yield treeToXML(currentSuite);
-  yield '\n</testsuites>\n';
+  yield '</testsuites>\n';
 };
--- packages/junit/tests/output.js
+++ packages/junit/tests/output.js
@@ -1,10 +1,10 @@
 module.exports = {
   stdout: `<\\?xml version="1.0" encoding="utf-8"\\?>
 <testsuites>
-  <testsuite name="tests" time=".*" disabled="0" errors="0" tests="2" failures="1" skipped="0" hostname=".*">
-    <testcase name="is ok" time=".*" classname="test"></testcase>
-    <testcase name="fails" time=".*" classname="test" failure="this is an error">
-      <failure message="this is an error" type="testCodeFailure">
+\t<testsuite name="tests" time=".*" disabled="0" errors="0" tests="2" failures="1" skipped="0" hostname=".*">
+\t\t<testcase name="is ok" time=".*" classname="test"/>
+\t\t<testcase name="fails" time=".*" classname="test" failure="this is an error">
+\t\t\t<failure type="testCodeFailure" message="this is an error">
 \\[Error \\[ERR_TEST_FAILURE\\]: this is an error\\] {
   failureType: 'testCodeFailure',
   cause: Error: this is an error
@@ -16,11 +16,12 @@ module.exports = {
       .*
       .*
       .*
+      .*
   code: 'ERR_TEST_FAILURE'
 }
-      </failure>
-    </testcase>
-  </testsuite>
+\t\t\t</failure>
+\t\t</testcase>
+\t</testsuite>
 </testsuites>
 `,
 };
