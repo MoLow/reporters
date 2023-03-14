8e90e64958ee4df3b3a60a2fab1f0c85914a295d
Moshe Atlow <moshe@atlow.co.il>
2023-03-14 17:12:28 +0200

    test: add coverage

--- packages/github/tests/output.js
+++ packages/github/tests/output.js
@@ -4,8 +4,11 @@ module.exports = {
 ::debug::completed running is ok
 ::debug::starting to run fails
 ::error title=fails,file=tests/example.js::\\[Error \\[ERR_TEST_FAILURE\\]: this is an error\\] {%0A  failureType: 'testCodeFailure',%0A  cause: Error: this is an error%0A      at Object.<anonymous> (.*/example.js:6:11).*
+::debug::starting to run is a diagnostic
+::debug::completed running is a diagnostic
+::notice file=tests/example.js::this is a diagnostic
 ::error title=tests,file=tests/example.js::\\[Error \\[ERR_TEST_FAILURE\\]: 1 subtest failed\\] { failureType: 'subtestsFailed', cause: '1 subtest failed', code: 'ERR_TEST_FAILURE' }
-::group::Test results \\(1 passed, 2 failed\\)
+::group::Test results \\(2 passed, 2 failed\\)
 ::notice::Total Tests: 1%0APassed ✅: 0%0AFailed ❌: 1%0ACanceled 🚫: 0%0ASkipped ⏭️: 0%0ATodo 📝: 0%0ADuration: .*ms
 ::endgroup::
 `,
--- tests/example.js
+++ tests/example.js
@@ -1,8 +1,9 @@
-const { describe, it } = require('node:test');
+const { describe, it, test } = require('node:test');
 
 describe('tests', () => {
   it('is ok', () => {});
   it('fails', () => {
     throw new Error('this is an error');
   });
+  test('is a diagnostic', async (t) => { t.diagnostic('this is a diagnostic'); });
 });
