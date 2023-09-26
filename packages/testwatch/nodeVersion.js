fb993b8c5e960b6986b1fdca4b754a2fdb22ea40
Moshe Atlow <moshe@atlow.co.il>
2023-09-26 10:06:46 +0300

    fix: node 20 skip text changes

--- packages/testwatch/nodeVersion.js
+++ packages/testwatch/nodeVersion.js
@@ -3,5 +3,7 @@
 const [major, minor] = process.versions.node.split('.').map(Number);
 
 module.exports = {
+  major,
+  minor,
   isSupported: major > 20 || (major === 20 && minor >= 3) || (major === 18 && minor >= 17),
 };
