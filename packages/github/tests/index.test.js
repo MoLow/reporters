47ab7c49b420365e2ba89f407c78e202084c68b7
Moshe Atlow <moshe@atlow.co.il>
2024-03-14 00:29:20 +0200

    test: migrate snapshots to snap

--- packages/github/tests/index.test.js
+++ packages/github/tests/index.test.js
@@ -4,47 +4,36 @@ const { test, describe, beforeEach } = require('node:test');
 const { spawnSync } = require('child_process');
 const { tmpdir } = require('os');
 const { join } = require('path');
-const assert = require('assert');
 const path = require('path');
 const { readFileSync, writeFileSync } = require('fs');
-const { compareLines } = require('../../../tests/utils');
-const output = require('./output');
-const outputESM = require('./output-esm');
+const { Snap, nodeMajor } = require('../../../tests/utils');
 
+const snapshot = Snap(`${__filename}.${nodeMajor}`);
 const GITHUB_STEP_SUMMARY = join(tmpdir(), 'github-actions-test-reporter');
-const nodeMajor = process.versions.node.split('.')[0];
 
 describe('github reporter', () => {
   beforeEach(() => {
     writeFileSync(GITHUB_STEP_SUMMARY, '');
   });
 
-  test('spawn with reporter', () => {
+  test('spawn with reporter', async () => {
     const child = spawnSync(process.execPath, ['--test-reporter', './index.js', '../../tests/example'], {
       env: { GITHUB_ACTIONS: true, GITHUB_STEP_SUMMARY, GITHUB_WORKSPACE: path.resolve(__dirname, '../../../') },
     });
 
-    assert.strictEqual(child.stderr?.toString(), '');
-    compareLines(child.stdout?.toString(), output.overrides[nodeMajor]?.stdout ?? output.stdout);
-    compareLines(readFileSync(GITHUB_STEP_SUMMARY).toString(), output.summary);
+    await snapshot(child, readFileSync(GITHUB_STEP_SUMMARY).toString('utf-8'));
   });
 
-  test('spawn with reporter - esm', () => {
+  test('spawn with reporter - esm', async () => {
     const child = spawnSync(process.execPath, ['--test-reporter', './index.js', '../../tests/example.mjs'], {
       env: { GITHUB_ACTIONS: true, GITHUB_STEP_SUMMARY, GITHUB_WORKSPACE: path.resolve(__dirname, '../../../') },
     });
 
-    assert.strictEqual(child.stderr?.toString(), '');
-    compareLines(
-      child.stdout?.toString(),
-      outputESM.overrides[nodeMajor]?.stdout ?? outputESM.stdout,
-    );
-    compareLines(readFileSync(GITHUB_STEP_SUMMARY).toString(), outputESM.summary);
+    await snapshot(child, readFileSync(GITHUB_STEP_SUMMARY).toString('utf-8'));
   });
 
-  test('should noop if not in github actions', () => {
+  test('should noop if not in github actions', async () => {
     const silentChild = spawnSync(process.execPath, ['--test-reporter', './index.js', '../../tests/example'], { env: { } });
-    assert.strictEqual(silentChild.stderr?.toString(), '');
-    assert.strictEqual(silentChild.stdout?.toString(), '');
+    await snapshot(silentChild);
   });
 });
